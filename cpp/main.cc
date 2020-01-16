bd69352642ebc2d52c21cb623f33953615851433
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-16 18:26:06 +0100

    added hello2

--- cpp/main.cc
+++ cpp/main.cc
@@ -16,6 +16,15 @@ napi_value hello(napi_env env, napi_callback_info args) {
   return greeting;
 }
 
+napi_value hello2(napi_env env, napi_callback_info args) {
+  napi_value greeting;
+  napi_status status;
+
+  status = napi_create_string_utf8(env, "world2", NAPI_AUTO_LENGTH, &greeting);
+  if (status != napi_ok) return nullptr;
+  return greeting;
+}
+
 napi_value init(napi_env env, napi_value exports) {
   napi_status status;
   napi_value fn;
@@ -25,6 +34,13 @@ napi_value init(napi_env env, napi_value exports) {
 
   status = napi_set_named_property(env, exports, "hello", fn);
   if (status != napi_ok) return nullptr;
+
+  status = napi_create_function(env, nullptr, 0, hello2, nullptr, &fn);
+  if (status != napi_ok) return nullptr;
+
+  status = napi_set_named_property(env, exports, "hello2", fn);
+  if (status != napi_ok) return nullptr;
+
   return exports;
 }
 
--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const sspi = require('bindings')('sspi');
 
 const result = sspi.hello();
 console.log(result);
+const result2 = sspi.hello2();
+console.log(result2);
