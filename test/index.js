910e63d720eb3550c527d3e9cc4b109b49cac9fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-17 17:15:18 +0100

    test e_AcquireCredentialsHandle

--- test/index.js
+++ test/index.js
@@ -3,5 +3,7 @@ const sspi = require('..');
 const result = sspi.hello();
 console.log(result);
 const result2 = sspi.EnumerateSecurityPackages();
+const result3 = sspi.AcquireCredentialsHandle();
 
 console.log(result2);
+console.log(result3);
