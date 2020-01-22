f4fdfe364a5cbc5fffe9550bda9b7f11960a066a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-22 09:40:47 +0100

    remove comment

--- misc/misc.js
+++ misc/misc.js
@@ -46,49 +46,4 @@ function printHexDump(buffer) {
     }
   }
   return result;
-
-  // DWORD i, count, index;
-  // CHAR rgbDigits[] = "0123456789abcdef";
-  // CHAR rgbLine[100];
-  // char cbLine;
-  // for (index = 0; length;
-  // 	length -= count, buffer += count, index += count)
-  // {
-  // 	count = (length > 16) ? 16 : length;
-  // 	sprintf_s(rgbLine, 100, "%4.4x  ", index);
-  // 	cbLine = 6;
-  // 	for (i = 0;i < count;i++)
-  // 	{
-  // 		rgbLine[cbLine++] = rgbDigits[buffer[i] >> 4];
-  // 		rgbLine[cbLine++] = rgbDigits[buffer[i] & 0x0f];
-  // 		if (i == 7)
-  // 		{
-  // 			rgbLine[cbLine++] = ':';
-  // 		}
-  // 		else
-  // 		{
-  // 			rgbLine[cbLine++] = ' ';
-  // 		}
-  // 	}
-  // 	for (; i < 16; i++)
-  // 	{
-  // 		rgbLine[cbLine++] = ' ';
-  // 		rgbLine[cbLine++] = ' ';
-  // 		rgbLine[cbLine++] = ' ';
-  // 	}
-  // 	rgbLine[cbLine++] = ' ';
-  // 	for (i = 0; i < count; i++)
-  // 	{
-  // 		if (buffer[i] < 32 || buffer[i] > 126)
-  // 		{
-  // 			rgbLine[cbLine++] = '.';
-  // 		}
-  // 		else
-  // 		{
-  // 			rgbLine[cbLine++] = buffer[i];
-  // 		}
-  // 	}
-  // 	rgbLine[cbLine++] = 0;
-  // 	printf("%s\n", rgbLine);
-  // }
 }
