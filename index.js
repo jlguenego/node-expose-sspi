5b0083a3cbd97589dd4ff10d69628b269e06106c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-01-24 19:09:54 +0100

    check os

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+if (require('os').platform() !== "win32") {
+    throw new Error("The module 'node-expose-sspi' can only work on Microsoft Windows platform.");
+}
+
 const sspi = require('bindings')('sspi');
 
 module.exports = sspi;
