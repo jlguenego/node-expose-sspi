98ba713f04dee2c054e1ebb8936482a0445add05
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-16 19:20:24 +0100

    error handling

--- cpp/api/GetUserName.cc
+++ cpp/api/GetUserName.cc
@@ -10,16 +10,12 @@ Napi::Value e_GetUserName(const Napi::CallbackInfo &info) {
 
   BOOL status = GetUserName(NULL, &cbBuffer);
   if (status == FALSE && GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
-    std::string msg = plf::string_format(
-        "GetUserName: error first call. (error code: 0x%08x)", GetLastError());
-    throw Napi::Error::New(env, msg);
+    throw Napi::Error::New(env, "GetUserName: error first call. " + plf::error_msg());
   }
   lpBuffer = (LPWSTR)malloc(cbBuffer * sizeof(WCHAR));
   status = GetUserName(lpBuffer, &cbBuffer);
   if (status == FALSE) {
-    std::string msg = plf::string_format(
-        "GetUserName: error second call. (error code: 0x%08x)", GetLastError());
-    throw Napi::Error::New(env, msg);
+    throw Napi::Error::New(env, "GetUserName: error second call. " + plf::error_msg());
   }
 
   Napi::String result = Napi::String::New(env, (char16_t *)lpBuffer);
