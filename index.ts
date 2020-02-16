6df8fcc59f3eb62b78a97f6aa0f31affe5d8e307
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-16 12:31:45 +0100

    remove import not used

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-import { Request, RequestHandler } from "express";
-
 if (require("os").platform() !== "win32") {
   throw new Error(
     "The module 'node-expose-sspi' can only work on Microsoft Windows platform."
