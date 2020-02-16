03a09e6698c407aafb3c67d60f3946333329bc37
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-02-16 11:41:03 +0100

    add doc

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { auth } from "./auth";
 import { connect } from "./connect";
 import { getDefaultDomain } from "./getDefaultDomain";
 
+// In CommonJS, default export must be written as 'export ='
 export = {
   auth,
   connect,
