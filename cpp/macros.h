cccc64eea163d7e705d515129b2a081895c7a12a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-23 18:12:24 +0100

    TypeError

--- cpp/macros.h
+++ cpp/macros.h
@@ -6,21 +6,21 @@
                            ##name " has failed: " + plf::ad_error_msg(hr)); \
   }
 
-#define CHECK_INPUT(msg, n)                             \
-  if (info.Length() != n) {                             \
-    throw Napi::Error::New(env, msg ": Bad arguments"); \
-  }
-
-#define CHECK_INPUT_DEFERRED(msg, n)                                       \
-  if (info.Length() != n) {                                                \
-    deferred.Reject(Napi::Error::New(env, msg ": Bad arguments").Value()); \
-    return deferred.Promise();                                             \
-  }
-
 #define AD_CHECK_ERROR_DEFERRED(hr, name)                                     \
   if (FAILED(hr)) {                                                           \
     deferred.Reject(                                                          \
         Napi::Error::New(env, ##name " has failed: " + plf::ad_error_msg(hr)) \
             .Value());                                                        \
     return deferred.Promise();                                                \
+  }
+
+#define CHECK_INPUT(msg, n)                                 \
+  if (info.Length() != n) {                                 \
+    throw Napi::TypeError::New(env, msg ": Bad arguments"); \
+  }
+
+#define CHECK_INPUT_DEFERRED(msg, n)                                           \
+  if (info.Length() != n) {                                                    \
+    deferred.Reject(Napi::TypeError::New(env, msg ": Bad arguments").Value()); \
+    return deferred.Promise();                                                 \
   }
