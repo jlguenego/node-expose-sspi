82708ce92216649817c443e11950203c0fae6150
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-25 10:29:27 +0100

    IADSGet improved

--- cpp/api/adsi/IADs/IADsGet.cc
+++ cpp/api/adsi/IADs/IADsGet.cc
@@ -8,13 +8,13 @@ class IADsGetWorker : public Napi::AsyncWorker {
  private:
   Napi::Promise::Deferred m_deferred;
   IADs *m_iads;
-  std::u16string m_input;
+  std::u16string m_name;
   std::u16string m_output;
 
  public:
   IADsGetWorker(Napi::Env &env, Napi::Promise::Deferred &deferred, IADs *iads,
-                std::u16string &input)
-      : AsyncWorker(env), m_deferred(deferred), m_iads(iads), m_input(input) {}
+                std::u16string &name)
+      : AsyncWorker(env), m_deferred(deferred), m_iads(iads), m_name(name) {}
 
   ~IADsGetWorker() {}
 
@@ -25,14 +25,13 @@ class IADsGetWorker : public Napi::AsyncWorker {
     VARIANT var;
     VariantInit(&var);
 
-    HRESULT hr = m_iads->Get(CComBSTR((const wchar_t *)m_input.c_str()), &var);
+    HRESULT hr = m_iads->Get(CComBSTR((const wchar_t *)m_name.c_str()), &var);
     if (FAILED(hr)) {
       return SetError("IADs.Get has failed. " + plf::ad_error_msg(hr));
     }
+
     BSTR bs = V_BSTR(&var);
-    std::wstring ws(bs, SysStringLen(bs));
-    const char16_t *str = (const char16_t *)ws.c_str();
-    m_output = str;
+    m_output = (const char16_t *)bs;
     VariantClear(&var);
   }
 
@@ -53,8 +52,8 @@ Napi::Value E_IADs::Get(const Napi::CallbackInfo &info) {
   auto deferred = Napi::Promise::Deferred::New(env);
   CHECK_INPUT_DEFERRED("IADs.Get(name: string)", 1);
 
-  std::u16string input = info[0].As<Napi::String>().Utf16Value();
-  IADsGetWorker *w = new IADsGetWorker(env, deferred, this->iads, input);
+  std::u16string name = info[0].As<Napi::String>().Utf16Value();
+  IADsGetWorker *w = new IADsGetWorker(env, deferred, this->iads, name);
   w->Queue();
   return deferred.Promise();
 }
