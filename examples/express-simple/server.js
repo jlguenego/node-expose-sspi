3c1e3d737c1c62b7068c1ff3756e7377d18843d8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-26 16:23:52 +0100

    updated simple example with the sso.init()

--- examples/express-simple/server.js
+++ examples/express-simple/server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const { sso } = require("node-expose-sspi");
+const express = require('express');
+const { sso } = require('../..');
 
 sso.config.debug = false;
 
 const app = express();
 
-app.use(sso.auth());
+(async () => {
+  await sso.init();
 
-app.use((req, res, next) => {
-  res.json({
-    sso: req.sso
+  app.use(sso.auth());
+
+  app.use((req, res, next) => {
+    res.json({
+      sso: req.sso,
+    });
   });
-});
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+  app.listen(3000, () => console.log('Server started on port 3000'));
+})();
