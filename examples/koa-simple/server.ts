1ffa76d57995bea9cb9e923faa95e9e732f2e2e9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-03-27 16:03:32 +0100

    koa working

---
+++ examples/koa-simple/server.ts
@@ -0,0 +1,16 @@
+import 'source-map-support/register';
+import Koa from 'koa';
+import { sso } from '../../src/index';
+const app = new Koa();
+
+const middleware = sso.auth();
+
+app.use(async (ctx, next) => {
+  await middleware(ctx.req as any, ctx.res as any, next);
+});
+
+app.use(async (ctx, next) => {
+  ctx.body = (ctx.req as any).sso;
+});
+
+app.listen(3000, () => console.log('Koa started on port 3000'));
