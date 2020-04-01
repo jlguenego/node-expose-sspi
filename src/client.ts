cf57964149b49c328a10ef981900e9037a69603f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-01 16:54:02 +0200

    add debug

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,16 @@
 import fetch, { RequestInit, Response } from 'node-fetch';
 import { sspi, InitializeSecurityContextInput } from '../lib/api';
 import { encode, decode } from 'base64-arraybuffer';
+import dbg from 'debug';
 
-async function handleAuth(response: Response, resource: string, init: RequestInit = {}): Promise<Response> {
+const debug = dbg('node-expose-sspi:handleAuth');
+
+async function handleAuth(
+  response: Response,
+  resource: string,
+  init: RequestInit = {}
+): Promise<Response> {
+  debug('handleAuth: start');
   if (!response.headers.has('www-authenticate')) {
     return response;
   }
@@ -15,27 +23,32 @@ async function handleAuth(response: Response, resource: string, init: RequestIni
 
   const clientCred = sspi.AcquireCredentialsHandle({
     packageName: 'Negotiate',
-    credentialUse: 'SECPKG_CRED_OUTBOUND'
+    credentialUse: 'SECPKG_CRED_OUTBOUND',
   });
   const packageInfo = sspi.QuerySecurityPackageInfo('Negotiate');
   let input: InitializeSecurityContextInput = {
     credential: clientCred.credential,
     targetName: 'kiki',
     cbMaxToken: packageInfo.cbMaxToken,
-    targetDataRep: 'SECURITY_NATIVE_DREP'
+    targetDataRep: 'SECURITY_NATIVE_DREP',
   };
   let clientSecurityContext = sspi.InitializeSecurityContext(input);
   // encode to Base64 and send via HTTP
   let base64 = encode(clientSecurityContext.SecBufferDesc.buffers[0]);
   let requestInit: RequestInit = { ...init };
-  requestInit.headers = { ...init.headers, authorization: 'Negotiate ' + base64 };
+  requestInit.headers = {
+    ...init.headers,
+    authorization: 'Negotiate ' + base64,
+  };
   response = await fetch(resource, requestInit);
   while (
     response.headers.has('www-authenticate') &&
     response.status === 401 &&
     response.headers.get('www-authenticate').startsWith('Negotiate ')
   ) {
-    const buffer = decode(response.headers.get('www-authenticate').substring('Negotiate '.length));
+    const buffer = decode(
+      response.headers.get('www-authenticate').substring('Negotiate '.length)
+    );
     input = {
       credential: clientCred.credential,
       targetName: 'kiki',
@@ -43,25 +56,30 @@ async function handleAuth(response: Response, resource: string, init: RequestIni
       serverSecurityContext: {
         SecBufferDesc: {
           ulVersion: 0,
-          buffers: [buffer]
-        }
+          buffers: [buffer],
+        },
       },
       contextHandle: clientSecurityContext.contextHandle,
-      targetDataRep: 'SECURITY_NATIVE_DREP'
+      targetDataRep: 'SECURITY_NATIVE_DREP',
     };
     clientSecurityContext = sspi.InitializeSecurityContext(input);
     base64 = encode(clientSecurityContext.SecBufferDesc.buffers[0]);
     requestInit = { ...init };
-    requestInit.headers = { ...init.headers, authorization: 'Negotiate ' + base64 };
+    requestInit.headers = {
+      ...init.headers,
+      authorization: 'Negotiate ' + base64,
+    };
     response = await fetch(resource, requestInit);
   }
+  debug('handleAuth: end');
   return response;
 }
 
 class Client {
   async fetch(resource: string, init?: RequestInit): Promise<Response> {
     const response = await fetch(resource, init);
-    return await handleAuth(response, resource, init);
+    const result = await handleAuth(response, resource, init);
+    return result;
   }
 }
 
