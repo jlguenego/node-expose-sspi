bfdedb604b5ab38a96ab1f8115b25b4e9feca0e7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-01 16:54:23 +0200

    better exception handling

--- src/userdb.ts
+++ src/userdb.ts
@@ -1,5 +1,5 @@
 import { adsi } from '../lib/api';
-import { IDirectorySearch } from '../lib/adsi';
+import { IDirectorySearch, IADs } from '../lib/adsi';
 import { isOnDomain } from './domain';
 import dbg from 'debug';
 import { Database, ADUser, ADUsers } from './interfaces';
@@ -63,35 +63,39 @@ export async function getUsers(): Promise<ADUsers> {
   if (!isOnDomain()) {
     return;
   }
-  adsi.CoInitializeEx(['COINIT_MULTITHREADED']);
-
-  const distinguishedName = await getDistinguishedName();
-  const dirsearch = await adsi.ADsOpenObject<IDirectorySearch>({
-    binding: `LDAP://${distinguishedName}`,
-    riid: 'IID_IDirectorySearch',
-  });
-  dirsearch.SetSearchPreference();
-  dirsearch.ExecuteSearch({
-    filter: '(&(objectClass=user)(objectCategory=person)(sn=*))',
-  });
-
   const result: ADUsers = [];
-  while (true) {
-    const row: ADUser = {};
-    const hr = dirsearch.GetNextRow();
-    if (hr === adsi.S_ADS_NOMORE_ROWS) {
-      break;
+  adsi.CoInitializeEx(['COINIT_MULTITHREADED']);
+  let dirsearch;
+  try {
+    const distinguishedName = await getDistinguishedName();
+    dirsearch = await adsi.ADsOpenObject<IDirectorySearch>({
+      binding: `LDAP://${distinguishedName}`,
+      riid: 'IID_IDirectorySearch',
+    });
+    dirsearch.SetSearchPreference();
+    dirsearch.ExecuteSearch({
+      filter: '(&(objectClass=user)(objectCategory=person)(sn=*))',
+    });
+
+    while (true) {
+      if (dirsearch.GetNextRow() === adsi.S_ADS_NOMORE_ROWS) {
+        break;
+      }
+      const row: ADUser = {};
+      let colName = dirsearch.GetNextColumnName();
+      while (colName !== adsi.S_ADS_NOMORE_COLUMNS) {
+        const value = await dirsearch.GetColumn(colName as string);
+        row[colName] = value;
+        colName = dirsearch.GetNextColumnName();
+      }
+      result.push(row);
     }
-    let colName = dirsearch.GetNextColumnName();
-    while (colName !== adsi.S_ADS_NOMORE_COLUMNS) {
-      const value = await dirsearch.GetColumn(colName as string);
-      row[colName] = value;
-      colName = dirsearch.GetNextColumnName();
-    }
-    result.push(row);
+  } catch (error) {
+    console.log('error: ', error);
+  } finally {
+    dirsearch && dirsearch.Release();
+    adsi.CoUninitialize();
   }
-  dirsearch.Release();
-  adsi.CoUninitialize();
   return result;
 }
 
@@ -99,7 +103,12 @@ export async function getDistinguishedName(): Promise<string> {
   if (!isOnDomain()) {
     return;
   }
-  const root = await adsi.ADsGestObject('LDAP://rootDSE');
-  const distinguishedName = root.Get('defaultNamingContext');
-  return distinguishedName;
+  let root: IADs;
+  try {
+    root = await adsi.ADsGestObject('LDAP://rootDSE');
+    const distinguishedName = await root.Get('defaultNamingContext');
+    return distinguishedName;
+  } finally {
+    root && root.Release();
+  }
 }
