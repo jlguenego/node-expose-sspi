5d0d5663d6fcbf9e7ffa37713252f111a8fc4e5b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-02 13:54:55 +0200

    refactor express-ejs

--- examples/express-ejs/server.ts
+++ examples/express-ejs/server.ts
@@ -1,90 +1,93 @@
-import express = require("express");
-import path = require("path");
-import session = require("express-session");
-import { sso, UserCredential } from "node-expose-sspi";
-import serveIndex = require("serve-index");
+import express = require('express');
+import path = require('path');
+import session = require('express-session');
+import { sso, UserCredential } from 'node-expose-sspi';
+import serveIndex = require('serve-index');
+import dbg from 'debug';
+
+const debug = dbg('node-expose-sspi:test');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.resolve(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "this is my super secreeeeeet!!!!!",
+    secret: 'this is my super secreeeeeet!!!!!',
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
-app.get("/", (req, res) => {
-  console.log("home");
-  return res.render("home", { user: req.session.user });
+app.get('/', (req, res) => {
+  debug('home');
+  return res.render('home', { user: req.session.user });
 });
 
-app.get("/login", (req, res) => {
-  console.log("login");
+app.get('/login', (req, res) => {
+  debug('login');
   const obj = { error: req.session.error };
   req.session.error = undefined;
-  res.render("login", obj);
+  res.render('login', obj);
 });
 
-app.post("/action/connect", async (req, res) => {
-  console.log("connect", req.body);
+app.post('/action/connect', async (req, res) => {
+  debug('connect', req.body);
   const domain = sso.getDefaultDomain();
-  console.log("domain: ", domain);
+  debug('domain: ', domain);
 
   const credentials: UserCredential = {
     domain,
     user: req.body.login,
-    password: req.body.password
+    password: req.body.password,
   };
-  console.log("credentials: ", credentials);
+  debug('credentials: ', credentials);
   const ssoObject = await sso.connect(credentials);
   if (ssoObject) {
     req.session.user = ssoObject.user;
-    return res.redirect("/protected/welcome");
+    return res.redirect('/protected/welcome');
   }
-  req.session.error = "bad login/password.";
-  return res.redirect("/login");
+  req.session.error = 'bad login/password.';
+  return res.redirect('/login');
 });
 
-app.get("/action/disconnect", (req, res) => {
-  console.log("disconnect");
+app.get('/action/disconnect', (req, res) => {
+  debug('disconnect');
   req.session.user = undefined;
-  return res.redirect("/");
+  return res.redirect('/');
 });
 
-app.get("/no-sso", (req, res) => {
-  return res.render("no-sso");
+app.get('/no-sso', (req, res) => {
+  return res.render('no-sso');
 });
 
-app.get("/sso", sso.auth(), (req, res) => {
-  console.log("sso");
+app.get('/sso', sso.auth(), (req, res) => {
+  debug('sso');
   if (!req.sso) {
-    return res.redirect("/no-sso");
+    return res.redirect('/no-sso');
   }
   req.session.user = req.sso.user;
-  return res.redirect("/protected/welcome");
+  return res.redirect('/protected/welcome');
 });
 
-app.use("/protected", (req, res, next) => {
-  console.log("protected");
+app.use('/protected', (req, res, next) => {
+  debug('protected');
   if (!req.session.user) {
-    return res.redirect("/login");
+    return res.redirect('/login');
   }
   next();
 });
 
-app.get("/protected/welcome", (req, res) => {
-  console.log("welcome", req.session.user);
-  return res.render("welcome", { user: req.session.user });
+app.get('/protected/welcome', (req, res) => {
+  debug('welcome', req.session.user);
+  return res.render('welcome', { user: req.session.user });
 });
 
-app.use(express.static(path.resolve(__dirname, ".")));
-app.use(serveIndex(path.resolve(__dirname, "."), { icons: true }));
+app.use(express.static(path.resolve(__dirname, '.')));
+app.use(serveIndex(path.resolve(__dirname, '.'), { icons: true }));
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(3000, () => console.log('Server started on port 3000'));
