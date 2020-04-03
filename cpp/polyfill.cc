d6043172cdfe41cd701211cf2a23e65019e3acd0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-03 17:52:24 +0200

    added polyfills

--- cpp/polyfill.cc
+++ cpp/polyfill.cc
@@ -1,7 +1,59 @@
 #include "polyfill.h"
+
+#include <codecvt>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 #include "adsi_error.h"
 
 namespace plf {
+
+// https://stackoverflow.com/questions/215963/how-do-you-properly-use-widechartomultibyte
+std::string wstrtostr(const std::wstring &wstr) {
+  // Convert a Unicode string to an ASCII string
+  std::string strTo;
+  char *szTo = new char[wstr.length() + 1];
+  szTo[wstr.size()] = '\0';
+  WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), -1, szTo, (int)wstr.length(),
+                      NULL, NULL);
+  strTo = szTo;
+  delete[] szTo;
+  return strTo;
+}
+
+std::wstring strtowstr(const std::string &str) {
+  // Convert an ASCII string to a Unicode String
+  std::wstring wstrTo;
+  wchar_t *wszTo = new wchar_t[str.length() + 1];
+  wszTo[str.size()] = L'\0';
+  MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, wszTo, (int)str.length());
+  wstrTo = wszTo;
+  delete[] wszTo;
+  return wstrTo;
+}
+
+// https://stackoverflow.com/questions/4358870/convert-wstring-to-string-encoded-in-utf-8
+std::string wstring_to_utf8(const std::wstring &wstr) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+  return myconv.to_bytes(wstr);
+}
+
+std::wstring utf8_to_wstring(const std::string &str) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+  return myconv.from_bytes(str);
+}
+
+std::string wstring_to_utf8(const wchar_t *buffer) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+  return myconv.to_bytes(buffer);
+}
+
+std::wstring utf8_to_wstring(const char *buffer) {
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
+  return myconv.from_bytes(buffer);
+}
 
 // See
 // https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
@@ -24,6 +76,11 @@ std::string string_format(const std::string fmt_str, ...) {
       break;
   }
   return std::string(formatted.get());
+}
+
+// same for std::u16string
+std::string string_format(const std::wstring &fmt_str, ...) {
+  return wstring_to_utf8(L"");
 }
 
 std::string error_msg(DWORD code) {
--- cpp/polyfill.h
+++ cpp/polyfill.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include <stdarg.h>  // For va_start, etc.
-#include <memory>    // For std::unique_ptr
-
-#include <windows.h>
 #include <stdio.h>
+#include <windows.h>
 
 #include <iostream>
-
+#include <memory>  // For std::unique_ptr
 #include <memory>
 #include <stdexcept>
 #include <string>
@@ -17,7 +15,15 @@
 
 namespace plf {
 // polyfills
+std::string wstring_to_utf8(const std::wstring &str);
+std::wstring utf8_to_wstring(const std::string &str);
+std::string wstring_to_utf8(const wchar_t *buffer);
+std::wstring utf8_to_wstring(const char *buffer);
+std::string wstrtostr(const std::wstring &wstr);
+std::wstring strtowstr(const std::string &str);
+
 std::string string_format(const std::string fmt_str, ...);
+std::string string_format(const std::wstring fmt_str, ...);
 std::string error_msg(DWORD code = 0);
 std::string ad_error_msg(HRESULT hr);
 
