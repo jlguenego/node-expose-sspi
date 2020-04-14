05d934164545047a6e8436e53a17868cbb8bc4ae
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-14 16:35:02 +0200

    crash scenario

---
+++ test/crash.spec.ts
@@ -0,0 +1,54 @@
+import {
+  adsi,
+  sysinfo,
+  sspi,
+  AcquireCredHandleInput,
+  AcceptSecurityContextInput,
+} from '../src';
+import a from 'assert';
+const assert = a.strict;
+import dbg from 'debug';
+import { sleep } from '../src/sso/sleep';
+import { IADs } from '../lib/adsi';
+const debug = dbg('node-expose-sspi:test');
+
+describe('CRASH Unit Test', function () {
+  this.timeout(15000);
+
+  it('should crash', async function () {
+    adsi.CoInitializeEx(['COINIT_MULTITHREADED']);
+
+    const packageInfo = sspi.QuerySecurityPackageInfo('Negotiate');
+
+    const clientCred = sspi.AcquireCredentialsHandle({
+      packageName: 'Negotiate',
+      credentialUse: 'SECPKG_CRED_BOTH',
+    });
+
+    const clientSecurityContext = sspi.InitializeSecurityContext({
+      credential: clientCred.credential,
+      targetName: 'kiki',
+      cbMaxToken: packageInfo.cbMaxToken,
+      targetDataRep: 'SECURITY_NATIVE_DREP',
+    });
+
+    adsi.ADsGestObject('LDAP://rootDSE').then(async (root: IADs) => {
+      const distinguishedName = await root.Get('defaultNamingContext');
+      debug('distinguishedName: ', distinguishedName);
+    });
+
+    setTimeout(() => {
+      const serverSecurityContext = sspi.AcceptSecurityContext({
+        credential: clientCred.credential,
+        clientSecurityContext,
+        contextReq: ['ASC_REQ_CONNECTION'],
+        targetDataRep: 'SECURITY_NATIVE_DREP',
+      });
+      debug('serverSecurityContext: ', serverSecurityContext);
+    }, 10);
+
+    debug('clientCred: ', clientCred);
+    adsi.CoUninitialize();
+    // await sleep(5000);
+  });
+});
