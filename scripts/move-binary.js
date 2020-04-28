484dc47f3b499926d515def9aafb127922877097
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-04-28 15:02:21 +0200

    feat: run on ia32 or x64 without any postinstall

--- scripts/move-binary.js
+++ scripts/move-binary.js
@@ -2,9 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 const projectDir = path.resolve(__dirname, '..');
+const orig = path.resolve(projectDir, './build/Release/api.node');
+const dest = path.resolve(projectDir, `./lib/arch/${process.arch}/api.node`);
 
-console.log('about to move');
-fs.renameSync(
-  path.resolve(projectDir, './build/Release/api.node'),
-  path.resolve(projectDir, './lib/api.node')
-);
+console.log(`about to move to: ${dest}`);
+fs.renameSync(orig, dest);
