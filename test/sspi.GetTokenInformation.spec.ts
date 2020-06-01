a76636026496b555a7dcf6b72975c8dfd86a95fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-01 11:32:56 +0200

    added unit test

---
+++ test/sspi.GetTokenInformation.spec.ts
@@ -0,0 +1,112 @@
+import { sspi, sso, AcquireCredHandleInput } from '../src';
+import os from 'os';
+import { strict as assert } from 'assert';
+import {
+  CredentialWithExpiry,
+  ServerSecurityContext,
+  SecurityContext,
+  InitializeSecurityContextInput,
+  AcceptSecurityContextInput,
+  Token,
+} from '../lib/sspi';
+
+describe('SSPI GetTokenInformation Unit Test', function () {
+  let clientCred: CredentialWithExpiry;
+  let serverCred: CredentialWithExpiry;
+  let clientSecurityContext: SecurityContext;
+  let serverSecurityContext: ServerSecurityContext;
+  let userToken: Token;
+
+  before(function () {
+    const acquireCredentialsHandleClientInput: AcquireCredHandleInput = {
+      packageName: 'Negotiate',
+      credentialUse: 'SECPKG_CRED_OUTBOUND',
+    };
+    const acquireCredentialsHandleServerInput: AcquireCredHandleInput = {
+      packageName: 'Negotiate',
+      credentialUse: 'SECPKG_CRED_INBOUND',
+    };
+    if (sso.isOnDomain()) {
+      acquireCredentialsHandleClientInput.authData = {
+        domain: os.hostname().toUpperCase(),
+        user: 'whatever',
+        password: 'guess',
+      };
+    }
+
+    const packageInfo = sspi.QuerySecurityPackageInfo('Negotiate');
+    clientCred = sspi.AcquireCredentialsHandle(
+      acquireCredentialsHandleClientInput
+    );
+    serverCred = sspi.AcquireCredentialsHandle(
+      acquireCredentialsHandleServerInput
+    );
+
+    const input: InitializeSecurityContextInput = {
+      credential: clientCred.credential,
+      targetName: 'kiki',
+      cbMaxToken: packageInfo.cbMaxToken,
+      targetDataRep: 'SECURITY_NATIVE_DREP',
+    };
+    clientSecurityContext = sspi.InitializeSecurityContext(input);
+
+    const serverInput: AcceptSecurityContextInput = {
+      credential: serverCred.credential,
+      clientSecurityContext,
+      contextReq: ['ASC_REQ_CONNECTION'],
+      targetDataRep: 'SECURITY_NATIVE_DREP',
+    };
+    serverSecurityContext = sspi.AcceptSecurityContext(serverInput);
+
+    const input2 = {
+      credential: clientCred.credential,
+      targetName: 'kiki',
+      cbMaxToken: packageInfo.cbMaxToken,
+      serverSecurityContext,
+      contextHandle: clientSecurityContext.contextHandle,
+    };
+    const clientSecurityContext2 = sspi.InitializeSecurityContext(input2);
+    const serverSecurityContext2 = sspi.AcceptSecurityContext({
+      credential: serverCred.credential,
+      clientSecurityContext: clientSecurityContext2,
+      contextHandle: serverSecurityContext.contextHandle,
+    });
+    assert(serverSecurityContext2.SECURITY_STATUS === 'SEC_E_OK');
+
+    sspi.ImpersonateSecurityContext(serverSecurityContext.contextHandle);
+    userToken = sspi.OpenThreadToken(['TOKEN_QUERY', 'TOKEN_QUERY_SOURCE']);
+    sspi.RevertSecurityContext(serverSecurityContext.contextHandle);
+  });
+
+  it('should test GetTokenInformation', function () {
+    const userGroups = sspi.GetTokenInformation({
+      accessToken: userToken,
+      tokenInformationClass: 'TokenGroups',
+    });
+    assert(typeof userGroups[0] === 'string');
+  });
+
+  it('should test GetTokenInformation with bad filter', function () {
+    const filter: any = true;
+    try {
+      sspi.GetTokenInformation({
+        accessToken: userToken,
+        tokenInformationClass: 'TokenGroups',
+        filter: filter as string,
+      });
+    } catch (error) {
+      console.log('error: ', error.message);
+      assert((error as Error).message.includes('filter must be a string'));
+    }
+  });
+
+  after(function () {
+    console.log('start after');
+    sspi.CloseHandle(userToken);
+    sspi.DeleteSecurityContext(serverSecurityContext.contextHandle);
+    sspi.DeleteSecurityContext(clientSecurityContext.contextHandle);
+    sspi.FreeCredentialsHandle(serverCred.credential);
+    sspi.FreeCredentialsHandle(clientCred.credential);
+    console.log('end after');
+  });
+});
