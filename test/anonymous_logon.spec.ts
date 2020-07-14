85ead766d59370cd2402be1dd57bf4af6c194916
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-14 13:50:20 +0200

    added anonymous logon file

---
+++ test/anonymous_logon.spec.ts
@@ -0,0 +1,7 @@
+import { strict as assert } from 'assert';
+
+describe('Anonymous logon', function () {
+  it('should connect with anonymous logon', function () {
+    assert.equal(true, true);
+  });
+});
