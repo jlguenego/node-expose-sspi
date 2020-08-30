2fc549b6e70cc6692cdd003e121e8bdf5b191fc3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-30 17:49:16 +0200

    fix: check version for issue #32

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import os from 'os';
 /* istanbul ignore if */
 if (os.platform() !== 'win32') {
   throw new Error(
-    "The module 'node-expose-sspi' can only work on Microsoft Windows platform."
+    "The module 'node-expose-sspi' only can work on Microsoft Windows platform."
+  );
+}
+
+/* istanbul ignore if */
+if (process.version < 'v12.16.1') {
+  throw new Error(
+    "The module 'node-expose-sspi' only can work on Node >= v.12.16.1."
   );
 }
 
