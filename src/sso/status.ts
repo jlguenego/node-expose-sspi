8111b439a23105c620ec8b962125c6eadcebc8c6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-09-12 12:29:02 +0200

    fix: try catch in getstatusinfo #37

--- src/sso/status.ts
+++ src/sso/status.ts
@@ -1,13 +1,19 @@
 import { sso } from './index';
 
 export function getStatusInfo(): any {
-  const result = {
-    adminPrivileges: sso.hasAdminPrivileges(),
-    isOnDomain: sso.isOnDomain(),
-    domain: sso.getDefaultDomain(),
-  } as any;
-  if (sso.isOnDomain()) {
-    result.isActiveDirectoryReachable = sso.isActiveDirectoryReachable();
+  try {
+    const result = {
+      adminPrivileges: sso.hasAdminPrivileges(),
+      isOnDomain: sso.isOnDomain(),
+      domain: sso.getDefaultDomain(),
+    } as any;
+    if (sso.isOnDomain()) {
+      result.isActiveDirectoryReachable = sso.isActiveDirectoryReachable();
+    }
+    return result;
+  } catch (e) {
+    return {
+      error: e,
+    };
   }
-  return result;
 }
