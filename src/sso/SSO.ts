d8f5b23637b6e176b6350ee3bb301011a8700fcd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-23 17:05:14 +0200

    added debug info

--- src/sso/SSO.ts
+++ src/sso/SSO.ts
@@ -36,14 +36,18 @@ export class SSO {
     const userToken = sspi.OpenThreadToken();
     debug('userToken: ', userToken);
     try {
+      debug('about to do GetUserNameEx');
       this.user.displayName = sspi.GetUserNameEx('NameDisplay');
     } catch (e) {
       // exemple of error scenario: local user without displayname.
       this.user.displayName = this.user.name;
     }
+    debug('about to do RevertSecurityContext');
     sspi.RevertSecurityContext(this.serverContextHandle);
 
+    debug('this.options.useGroups: ', this.options.useGroups);
     if (this.options.useGroups) {
+      debug('about to do GetTokenInformation');
       const groups = sspi.GetTokenInformation({
         accessToken: userToken,
         tokenInformationClass: 'TokenGroups',
@@ -55,18 +59,22 @@ export class SSO {
     }
 
     // free the userToken
+    debug('about to do CloseHandle');
     sspi.CloseHandle(userToken);
 
+    debug('about to do LookupAccountName');
     const { sid } = sspi.LookupAccountName(names.sUserName);
     this.user.sid = sid;
 
     try {
+      debug('about to do isOnDomain and isActiveDirectoryReachable');
       if (
         sso.isOnDomain() &&
         sso.isActiveDirectoryReachable() &&
         os.hostname() !== domain &&
         this.options.useActiveDirectory
       ) {
+        debug('about to do getUser');
         const adUser = await getUser(`(sAMAccountName=${name})`);
         this.user.adUser = adUser;
       }
@@ -75,22 +83,28 @@ export class SSO {
     }
 
     // owner info.
+    debug('this.options.useOwner: ', this.options.useOwner);
     if (this.options.useOwner) {
+      debug('about to do GetUserName');
       const owner = sspi.GetUserName();
       debug('owner: ', owner);
       this.owner = { name: owner };
       try {
+        debug('about to do GetUserNameEx');
         this.owner.displayName = sspi.GetUserNameEx('NameDisplay');
       } catch (e) {
         // exemple of error scenario: local user without displayname.
         this.owner.displayName = this.owner.name;
       }
 
+      debug('this.options.useGroups: ', this.options.useGroups);
       if (this.options.useGroups) {
+        debug('about to do OpenProcessToken');
         const processToken = sspi.OpenProcessToken([
           'TOKEN_QUERY',
           'TOKEN_QUERY_SOURCE',
         ]);
+        debug('about to do GetTokenInformation');
         const ownerGroups = sspi.GetTokenInformation({
           accessToken: processToken,
           tokenInformationClass: 'TokenGroups',
@@ -99,14 +113,19 @@ export class SSO {
         ownerGroups.sort();
         debug('ownerGroups: ', ownerGroups);
         this.owner.groups = ownerGroups;
+        debug('about to do CloseHandle');
         sspi.CloseHandle(processToken);
       }
 
       try {
+        debug('about to do LookupAccountName');
         const o = sspi.LookupAccountName(owner);
         this.owner.sid = o.sid;
         this.owner.domain = o.domain;
-      } catch (e) {}
+      } catch (e) {
+        debug('error: ', e);
+      }
+      debug('SSO end.');
     }
   }
 
